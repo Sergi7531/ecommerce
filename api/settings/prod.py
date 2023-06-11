cccc3323e02c8d6adbc2663ede870356b99824ec
Sergi7531 <sergidominguezrivas@gmail.com>
2023-06-11 11:41:43 +0200

    Removed admin-ish interface for DRF views on browser

--- api/settings/prod.py
+++ api/settings/prod.py
@@ -2,4 +2,10 @@ from .base import *
 
 DEBUG = False
 
+REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+    )
+}
+
 ALLOWED_HOSTS = ['77.227.163.99']
