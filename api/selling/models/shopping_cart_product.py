75553d43d4b3d58f9c21f00b22f2f878d0b8d960
Sergi7531 <sergidominguezrivas@gmail.com>
2024-01-12 01:35:51 +0100

    Missed package spec to foreign key

--- api/selling/models/shopping_cart_product.py
+++ api/selling/models/shopping_cart_product.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class ShoppingCartProduct(models.Model):
-    cart = models.ForeignKey('ShoppingCart', on_delete=models.CASCADE)
-    product = models.ForeignKey('Product', related_name='products', on_delete=models.CASCADE)
-    sizing = models.ForeignKey('Sizing', on_delete=models.CASCADE)
+    cart = models.ForeignKey('selling.ShoppingCart', on_delete=models.CASCADE)
+    product = models.ForeignKey('selling.Product', related_name='products', on_delete=models.CASCADE)
+    sizing = models.ForeignKey('selling.Sizing', on_delete=models.CASCADE)
     amount = models.IntegerField()
