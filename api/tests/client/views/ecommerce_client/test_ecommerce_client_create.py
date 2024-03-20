6882374a1a0fd4de60b9021c479313937e0b82d6
Sergi7531 <sergidominguezrivas@gmail.com>
2024-03-20 01:07:36 +0100

    100% coverage for clients/ POST (new client) endpoint

--- api/tests/client/views/ecommerce_client/test_ecommerce_client_create.py
+++ api/tests/client/views/ecommerce_client/test_ecommerce_client_create.py
@@ -52,6 +52,8 @@ class TestEcommerceClient:
         assert response.status_code == HTTP_400_BAD_REQUEST
 
     def test_ecommerce_client_create_email_in_use(self, api_client: APIClient) -> NoReturn:
+        _ = api_client.post(self.url, data=self._ecommerce_client_creation_data)
+
         response = api_client.post(self.url, data=self._ecommerce_client_creation_data)
 
         assert response.status_code == status.HTTP_400_BAD_REQUEST
