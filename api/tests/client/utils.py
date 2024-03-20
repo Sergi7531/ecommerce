28c1b68c05b02689f332d3b9afe5dc6db9ccd94b
Sergi7531 <sergidominguezrivas@gmail.com>
2024-03-20 01:46:27 +0100

    Added missing test cases for ecommerce client creation endpoint (POST new client)

--- api/tests/client/utils.py
+++ api/tests/client/utils.py
@@ -1,11 +1,14 @@
 import json
 from functools import wraps
+from typing import Optional
 
-def authorized_test(func):
+
+def authorized_test(func, email: Optional[str] = None, password: Optional[str] = None):
     @wraps(func)
     def wrapper(self, api_client, *args, **kwargs):
-        response = api_client.post(self._login_url, data={"email": 'test123456789@example.com',
-                                                           "password": 'testing_password123'})
+        _email = email or 'test123456789@example.com'
+        _password = password or 'testing_password123'
+        response = api_client.post(self._login_url, data={"email": _email, "password": _password})
 
         api_client.credentials(HTTP_AUTHORIZATION=f'Token {json.loads(response.content)["token"]}')
         return func(self, api_client, *args, **kwargs)
